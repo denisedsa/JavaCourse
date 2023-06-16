f7e3ae4e79d5561d12c39a651c83e10c1320b087
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2023-06-16 07:21:11 -0300

    Add files via upload

---
+++ EstruturaCondicional.java
@@ -0,0 +1,31 @@
+package estruturacondicional;
+
+public class EstruturaCondicional {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        // >
+        // >=
+        // <
+        // <=
+        // ==
+        // !=
+        // &&
+        // || (shift + barra invertida)
+        // !
+        
+    int media = 7;
+        if(media >= 7){
+            if (media == 10){
+                //APTOVADO COM NOTA MAXIMA
+            } else {
+                //APROVADO
+            }
+        } else {
+            //REPROVADO
+    )
+    
+    }
+    
+}
