ab5b67f3d3d642aadf0eb4955151c848b22dba50
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2023-06-16 07:22:04 -0300

    Add files via upload

---
+++ OrientaçãoObjeto.java
@@ -0,0 +1,20 @@
+package com.mycompany.estrutura_escolha;
+
+import java.util.Scanner;
+public class NewClassPessoas {
+    
+public float void main(String[] args){
+
+Pessoa objetoPessoa = new Pesso().    
+Scanner leitor = new (System.in);   
+
+Systxem.out.println("Digite a altura da pessoa");   
+  objetoPessoa.setPeso(eitor.nextFloat()));
+System.out.println("IMC = " + imc");      
+  objetoPessoa.setAltura (eitor.nextFloat()));
+
+  System.out.println("IMC = " + objetoPessoa.calcularIMC());
+  
+    }
+
+}
