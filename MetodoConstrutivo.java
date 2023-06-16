de9cdb6a3a74edb8896134467f183aaab02ed125
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2023-06-16 07:22:36 -0300

    Add files via upload

---
+++ MetodoConstrutivo.java
@@ -0,0 +1,21 @@
+package Encapsulamento;
+
+public class Encapsulamento {
+    
+    //Metodos construtor
+    
+    Pessoa objetoPessoa = new Pessoa()
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Digite o peso da pessoa");   
+    objetoPessoa.setPeso (Leitor.nextFloat();
+    System.out.println("Digite a altura da pessoa");   
+    objetoPessoa.setAltura (Leitor.nextFloat();    
+    
+    System.out.println("IMC = " + objetoPessoa.calcularIMC());
+
+
+    }
+
+}
+
