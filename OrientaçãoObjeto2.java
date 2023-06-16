ec627aa3aa9afd8dbc762554837fa7e13f3118dc
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2023-06-16 07:22:55 -0300

    Add files via upload

---
+++ OrientaçãoObjeto2.java
@@ -0,0 +1,20 @@
+package com.mycompany.estrutura_escolha;
+
+import java.util.Scanner;
+public class NewClassPessoas {
+   
+    public static void main {String[] args)[]
+   
+    Pessoa objetoPessoa = new Pessoa();
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Digite o peso da pessoa");   
+        objetoPessoa.setpeso(leitor.nextFloat());
+
+System.out.println("Digite a altura da pessoa");   
+        objetoPessoa.setAltura(leitor.nextFloat(());
+        
+        System.out.println("IMC = " + objetoPessoaIMC");   
+      
+    }
+}
