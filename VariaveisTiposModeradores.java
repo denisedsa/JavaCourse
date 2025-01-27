9cf90bbb50b8879acb7ced8d66f31b836be29c07
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2025-01-27 12:20:11 -0300

    Update VariaveisTiposModeradores.java

--- VariaveisTiposModeradores.java
+++ VariaveisTiposModeradores.java
@@ -1,10 +1,15 @@
-
 package com.mycompany.variaveistiposmoderadores;
 
 public class VariaveisTiposModeradores {
 
     public static void main(String[] args) {
+        // Exibe a mensagem no console
         System.out.println("Hello World!");
-        int 
+
+        // Declaração de uma variável do tipo int
+        int numero = 10; // A variável 'numero' é do tipo inteiro e recebe o valor 10
+
+        // Exibindo o valor da variável 'numero'
+        System.out.println("O valor da variável numero é: " + numero);
     }
 }
