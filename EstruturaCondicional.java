5c66a042f58a415dc10813a380d545029ff226a1
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2025-01-27 12:15:06 -0300

    Update EstruturaCondicional.java

--- EstruturaCondicional.java
+++ EstruturaCondicional.java
@@ -3,29 +3,29 @@ package estruturacondicional;
 public class EstruturaCondicional {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-   
-        // >
-        // >=
-        // <
-        // <=
-        // ==
-        // !=
-        // &&
-        // || (shift + barra invertida)
-        // !
-        
-    int media = 7;
-        if(media >= 7){
-            if (media == 10){
-                //APTOVADO COM NOTA MAXIMA
+        // Exemplo de operadores condicionais:
+        // >  (maior que)
+        // >= (maior ou igual)
+        // <  (menor que)
+        // <= (menor ou igual)
+        // == (igual)
+        // != (diferente)
+        // && (e lógico)
+        // || (ou lógico)
+        // !  (não lógico)
+
+        // Variável de exemplo
+        int media = 7;
+
+        // Estrutura condicional para avaliar a média
+        if (media >= 7) { // Aprovado
+            if (media == 10) {
+                System.out.println("APROVADO COM NOTA MÁXIMA!");
             } else {
-                //APROVADO
+                System.out.println("APROVADO");
             }
-        } else {
-            //REPROVADO
-    )
-    
+        } else { // Reprovado
+            System.out.println("REPROVADO");
+        }
     }
-    
 }
