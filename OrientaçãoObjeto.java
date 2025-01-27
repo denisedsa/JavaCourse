999b432f873110fddb6bfb4f3c82347e5223b97d
Denise Almeida <90991366+denisedsa@users.noreply.github.com>
2025-01-27 12:18:48 -0300

    Update OrientaçãoObjeto.java

--- OrientaçãoObjeto.java
+++ OrientaçãoObjeto.java
@@ -1,20 +1,31 @@
 package com.mycompany.estrutura_escolha;
 
-import java.util.Scanner;
-public class NewClassPessoas {
-    
-public float void main(String[] args){
+public class Pessoa {
+    private float peso;
+    private float altura;
 
-Pessoa objetoPessoa = new Pesso().    
-Scanner leitor = new (System.in);   
-
-Systxem.out.println("Digite a altura da pessoa");   
-  objetoPessoa.setPeso(eitor.nextFloat()));
-System.out.println("IMC = " + imc");      
-  objetoPessoa.setAltura (eitor.nextFloat()));
-
-  System.out.println("IMC = " + objetoPessoa.calcularIMC());
-  
+    // Métodos setters e getters
+    public float getPeso() {
+        return peso;
     }
 
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    // Método para calcular o IMC
+    public float calcularIMC() {
+        if (altura > 0) {
+            return peso / (altura * altura); // Fórmula para calcular o IMC
+        }
+        return 0; // Evita divisão por zero caso altura seja 0
+    }
 }
